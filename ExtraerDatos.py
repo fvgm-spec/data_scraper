678a8ad73b20b45b092052818a26a29c359344da
Felix Gutierrez <60470663+fvgm-spec@users.noreply.github.com>
2021-02-20 17:52:41 -0300

    Update ExtraerDatos.py

--- ExtraerDatos.py
+++ ExtraerDatos.py
@@ -7,39 +7,47 @@ import datetime
 
 '''
 Este script se conecta a la pagina web de Google Apps Store que el usuario haya seleccionado
-y extrae una lista de las los datas correspondientes al nombre de usuario, comentario y 
-fecha del copmentario'''
+y extrae una lista de las los datos correspondientes al nombre de usuario, comentario y 
+fecha del comentario'''
 
 driver = webdriver.Firefox(executable_path = 'C:\\WebDrivers\\geckodriver.exe')
 base_url = input("Escribir la URL de la App: ")
 url = (base_url)
 driver.get(url)
-time.sleep(1)
+time.sleep(120)
 
-lista = [ ]
-#Realiza una lista de los datos extraidos de la pagina seleccionada
+nombres = [ ]
 names = driver.find_elements_by_xpath("//span[contains(@class,'X43Kjb')]")
 for name in names:
     n = name.text
-    lista.append(n)
+    nombres.append(n)
+
+comentarios = [ ]
 comments = driver.find_elements_by_xpath("//span[contains(@jsname,'bN97Pc')]")
 for comment in comments:
     c = comment.text
-    lista.append(c)
+    comentarios.append(c)
 
+fechas = [ ]
 dates = driver.find_elements_by_xpath("//span[contains(@class,'p2TkOb')]")
 for date in dates:
     d = date.text
-    lista.append(d)
+    fechas.append(d)
 
 #Abre el archivo excel correspondiente a las aplicaciones extraidas
-book = Workbook()
-sheet = book.active
+wb = Workbook()
+wb_name = 'Applications.xlsx'
+sheet = wb.active
+sheet.title = "apps"
+row = 1
+col_names = 1 #columna donde se escriben los nombres de usuario
+col_comm = 2 #columna donde se escriben los comentarios de usuario
+col_dates = 3 #columna donde se escriben las fechas
 
-workbook_name = 'Applications.xlsx'
-wb = load_workbook(workbook_name)
-page = wb.active
+for nombre, comentario, fecha in zip(nombres, comentarios, fechas):
+    sheet.cell(column=col_names, row=row, value=nombre)
+    sheet.cell(column=col_comm, row=row, value=comentario)
+    sheet.cell(column=col_dates, row=row, value=fecha)
+    row+=1
 
-page.append(lista)
-
-wb.save(filename=workbook_name)
+wb.save(filename=wb_name)
