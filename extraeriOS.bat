8af3eee41415e6284a7d57b54196782bd3e0592f
Felix Gutierrez <60470663+fvgm-spec@users.noreply.github.com>
2021-02-21 21:02:55 -0300

    Adding new file

---
+++ extraeriOS.bat
@@ -0,0 +1,4 @@
+@echo off
+@echo Starting Script at %date% %time% >> %logfile%
+"C:\Users\felix\AppData\Local\Microsoft\WindowsApps\python.exe" "C:\Users\Desktop\entregable\extraerdatosiOS.py"
+@echo finished at %date% %time% >> %logfile%
